228eeab239b5abc2824d19099be55747e1d588f1
Munu <lisa.friend3@gmail.com>
2017-12-15 16:04:24 +0530

    .

---
+++ client/src/app/components/login/login.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthenticationService} from "../../services/authentication.service";
+import {Router} from "@angular/router";
+import {Users} from "../../models/users";
+import {pathName} from "../../services/common";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../../app.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  model: Users = new Users;
+  loading = false;
+  error = '';
+  pathName: string = pathName;
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService) {
+  }
+
+  ngOnInit() {
+    // reset login status
+    this.authenticationService.logout();
+  }
+
+  login() {
+    this.loading = true;
+    this.authenticationService.login(this.model)
+      .subscribe(result => {
+        console.log(result + "/////");
+        if (result === true) {
+          setTimeout(() => {
+            this.router.navigate(['/']);
+          }, 1500);
+        } else {
+          this.error = 'UserId or password is incorrect';
+          this.loading = false;
+        }
+      });
+  }
+
+}
